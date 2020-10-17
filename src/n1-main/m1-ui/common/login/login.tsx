dda111f9410b138e41f2175532e2654377a468db
InHuman66 <olexandr.kk@gmail.com>
2020-10-17 13:41:54 +0300

    HTML coding and css DONE

--- src/n1-main/m1-ui/common/login/login.tsx
+++ src/n1-main/m1-ui/common/login/login.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import classes from "./login.module.css";
+import {NavLink} from "react-router-dom";
 
 const Login= () => {
 
     return (
         <div className={classes.beckGround}>
-
+            <div className={classes.logIn_menu}>
+                <h1>Sign-In</h1>
+                <div className={classes.menu_wrapper}>
+                    <div className={classes.input_wrapper}>
+                        <input placeholder={'your email'}/>
+                    </div>
+                    <div className={classes.input_wrapper}>
+                        <input placeholder={'your password'}/>
+                    </div>
+                    <NavLink className={classes.Link} to={'/password-recovery'} activeClassName={classes.activeNavlink}>Forgot password ?</NavLink>
+                    <div className={classes.checkBox_wrapper}>
+                        <input type={'checkbox'}/>
+                        <p>Remember Me</p>
+                    </div>
+                    <button className={classes.button_signIn}>Sign In</button>
+                    <NavLink className={classes.Link} to={'/registration'} activeClassName={classes.activeNavlink}>Registration</NavLink>
+                </div>
+            </div>
         </div>
     );
 }
