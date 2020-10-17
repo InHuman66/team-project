27e6a7ec24a095c3ee852a85dc30452ae2862945
InHuman66 <olexandr.kk@gmail.com>
2020-10-17 11:56:13 +0300

    New header most beautiful=)

--- src/n1-main/m1-ui/common/navbar/navbar.tsx
+++ src/n1-main/m1-ui/common/navbar/navbar.tsx
@@ -1,16 +1,18 @@
 import { NavLink } from "react-router-dom"
 import React from 'react';
-
+import classes from "./navbar.module.css";
 
 
 export const Navbar = () => {
     return (
-        <div>
-            <NavLink to={'/login'}>Log In</NavLink>
-            <NavLink to={'/new-password'}>NewPassword</NavLink>
-            <NavLink to={'/password-recovery'}>PasswordRecovery</NavLink>
-            <NavLink to={'/profile'}>Profile</NavLink>
-            <NavLink to={'/registration'}>Registration</NavLink>
+        <div className={classes.headerBackground}>
+            <div className={classes.blockLinks}>
+                <NavLink className={classes.Link} to={'/login'} activeClassName={classes.activeNavlink} >Log In</NavLink>
+                <NavLink className={classes.Link} to={'/new-password'} activeClassName={classes.activeNavlink}>NewPassword</NavLink>
+                <NavLink className={classes.Link} to={'/password-recovery'} activeClassName={classes.activeNavlink}>PasswordRecovery</NavLink>
+                <NavLink className={classes.Link} to={'/profile'} activeClassName={classes.activeNavlink}>Profile</NavLink>
+                <NavLink className={classes.Link} to={'/registration'} activeClassName={classes.activeNavlink}>Registration</NavLink>
+            </div>
         </div>
 
     )
